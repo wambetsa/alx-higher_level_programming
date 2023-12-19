357d0970a674bf4755e69e9109a31904e24be418
wambetsa <wambetsae@gmail.com>
2023-12-19 08:34:38 -0800

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    result = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            result += 1
+        except IndexError:
+            pass
+    print()            
+    return result
