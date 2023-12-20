8efb2fa86b46c74596ff6c8e3289ab4c65691794
wambetsa emmanuel <wambetsae@gmail.com>
2023-12-21 00:47:06 +0300

    square class with private class attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Represents class square"""
+
+
+class Square:
+    """Represents empty Square"""
+    pass
+
+    def __init__(self, size):
+        """Initialization of the data."""
+        self.__size = size
