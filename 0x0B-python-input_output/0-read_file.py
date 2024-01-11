b176a0494b26cc91d5ee0120c0635bdc1dd3dbca
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-11 14:10:06 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-Read file function
+    function that reads a text file (UTF8)
+    and prints it to stdout:
+
+    Prototype: def read_file(filename=""):
+    You must use the with statement
+    You don’t need to manage file permission or
+    file doesn't exist exceptions.
 """
 
 
 def read_file(filename=""):
-    """read a file
-    args:
-        filename: file to manipulate
-    return:
-        na
-    """
-
-    with open(filename, encoding="utf-8") as f:
-        for line in f.read():
-            print(line, end="")
+    """ read a file """
+    if filename:
+        with open(filename, mode="r", encoding='utf-8') as file:
+            for line in file:
+                print(line, end="")
