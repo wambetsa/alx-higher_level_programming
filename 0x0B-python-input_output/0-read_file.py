70cb878877aa8603488f9cb62d86ef5665ef2717
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-11 14:06:46 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+Read file function
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """read a file
+    args:
+        filename: file to manipulate
+    return:
+        na
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        for line in f.read():
+            print(line, end="")
