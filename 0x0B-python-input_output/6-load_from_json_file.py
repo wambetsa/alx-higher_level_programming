28134bc88bb23c2d68f8bb8f8a58d03229e6d6d1
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-12 19:49:24 +0300

    a function that creates an Object from a “JSON file”:

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining a load from json file function"""
+import json
+
+
+def load_from_file(filename):
+    """a function that creates an Object from a “JSON file”:
+    Arg:
+        filename
+    Return:
+        object
+    """
+    with open(filename, 'r', encoding='utf-8') as fp:
+        return json.load(fp)
