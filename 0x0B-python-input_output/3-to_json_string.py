dc743564ae52b5218e1524e0e1e65156e0542fe4
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-12 19:10:49 +0300

    returns json representation of an object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,15 @@
 #!usr/bin/python3
-"""Defining a function that returns a json string"""
+"""
+    Defining a function that returns a json string
+"""
+import json
 
 
 def to_json_string(my_obj):
-    import json
-    """a function that returns the JSON 
-    representation of an object (string)
+    """a function that returns the JSON representation of object
+    Arg:
+        my_obj
+    return:
+        string
     """
     return json.dumps(my_obj)
