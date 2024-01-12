5c9d8318f340a06884e0cded383832802aff10a3
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-12 18:45:44 +0300

    write to text to filename

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""a function that writes a string to a text file (UTF8) and 
-returns the number of characters written
-"""
+"""Defines a file writing function"""
 
 
 def write_file(filename="", text=""):
     """open file with name filepath and write text into our filename"""
     with open(filename, 'w', encoding='utf-8') as fp:
-        fp.write(text)
+        return fp.write(text)
