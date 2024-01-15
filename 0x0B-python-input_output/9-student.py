8bab1f3653859f2b1548f91b041d9bfd8009c875
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-15 14:50:00 +0300

    Class Student to dict representation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,11 +3,12 @@
 
 
 class Student:
-    """constructor"""
+    """constructor representing Student"""
     def __init__(self, first_name, last_name, age):
+        """student initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     def to_json(self):
         """dict representation of student"""
-        return self.__dict__(self)
+        return self.__dict__
