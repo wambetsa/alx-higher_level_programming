dbd77aa2d6c4438e43ee65a4c6040b49c5ee37bc
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-15 15:27:52 +0300

    search string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-'''append_after'''
-
+"""append_after"""
 
 def append_after(filename="", search_string="", new_string=""):
-	'''search and update'''
+	"""search and update"""
 	read = []
 	with open(filename, "r", encoding="utf-8") as f:
 		read = f.readlines()
