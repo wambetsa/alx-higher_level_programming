c474120d2a5ea6e3674823b0026376b327887047
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-15 15:05:55 +0300

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining pascal_triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangles = [[1]]
+    while len(triangles) != n:
+        tr = triangles[-1]
+        tmp = [1]
+        for i in range(len(tr) - 1):
+            tmp.append(tr[i] + tr[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
