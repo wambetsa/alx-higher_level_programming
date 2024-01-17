23993930482cd5883e8865ba3a4d8e5d675230f7
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-17 15:02:48 +0300

    Rectanle raise exceptions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""import Base class from models.base"""
-from models.base import Base
+"""import Base class from base file"""
+from base import Base
 
 
 class Rectangle(Base):
 
     """constructor/Initializing class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """instance attributes"""
+        """instance attributes
+        Args: height: int, width : int, x : int, y : int
+        Raises: TypeError, ValueError
+        """
         """inherit id from base class"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
     
     """height getter"""
     @property
@@ -23,6 +26,10 @@ class Rectangle(Base):
     """height setter"""
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
     
     """width getter"""
@@ -33,6 +40,10 @@ class Rectangle(Base):
     """width setter"""
     @width.setter
     def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
     
     """x getter"""
@@ -42,6 +53,10 @@ class Rectangle(Base):
     
     @x.setter
     def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
     
     """y getter"""
@@ -52,5 +67,8 @@ class Rectangle(Base):
     """y setter"""
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
-    
