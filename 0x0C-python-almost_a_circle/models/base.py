00f76daf0a95d3327487ea41cf0816cca568fd68
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-17 15:18:03 +0300

    2. First rectangle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,21 +13,3 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-#usage of above class
-if __name__ == "__main__":
-
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
