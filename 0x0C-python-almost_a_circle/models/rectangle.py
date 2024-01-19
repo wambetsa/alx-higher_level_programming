e75382f147a1a14944750ad0408134fb7a6c813c
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-20 00:45:49 +0300

    change update function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,7 +89,17 @@ class Rectangle(Base):
             # for j in range(self.__width):
             #     print("{}".format(res), end='')
             print(end='\n')
-    def update(self, *args):
+    def update(self, *args, **kwargs):
+        """Rectangle Update
+        Args:
+            *args (integers): New attribute values.
+                - 1st arg reps id attribute
+                - 2nd arg reps width attribute
+                - 3rd arg reps height attribute
+                - 4th arg reps x attribute
+                - 5th arg reps y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args and len(args) != 0:
             a = 0
             for i in args:
@@ -107,4 +117,36 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = i
                 a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+        def to_dictionary(self):
+            """Return the dictionary representation of a Rectangle."""
+            return {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+            }
+
+        def __str__(self):
+            """Return the print() and str() representation of the Rectangle."""
+            return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                        self.x, self.y,
+                                                        self.width, self.height)
 
