2e5d80cf476d467b95e8af0f38fcedd0602dc9ad
wambetsa <wambetsae@gmail.com>
2024-02-14 05:42:42 -0800

    Show create table tablename

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- a script that prints full description of table first_table from database hbtn_0c_0
+-- database name is passed as argument in mysql command
+-- no DESCRIBE and EXPLAIN statements
+SHOW CREATE TABLE `first_table`
