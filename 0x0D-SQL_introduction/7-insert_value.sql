87628e908a73ec1d5bcff71f7fed9af6dcc4fadd
wambetsa <wambetsae@gmail.com>
2024-02-14 06:10:15 -0800

    insert into first_name (id, name) values (89, best school):

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- sql script that inserts a new row in table first_table in database hbtn_0c_0
+-- new row details: id = 89, name = best school
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
