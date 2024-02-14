9285ec34b36c75f61bb90f4cfa9fe3d7a6d0e101
wambetsa <wambetsae@gmail.com>
2024-02-14 08:50:38 -0800

    Insert multiple values in database table

--- 9-full_creation.sql
+++ 9-full_creation.sql
@@ -1,6 +1,3 @@
--- a sql script that creates second_table in database hbtn_0c_0 and add multiples rows
+-- a sql script that creates second_table in database hbtn_0c_0 and add multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
