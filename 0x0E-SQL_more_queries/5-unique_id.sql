ad79ffa353de337af691b92ba0a2c2420e2ac5c6
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-15 02:10:09 +0300

    sql more queries

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -3,6 +3,7 @@
 --id INT with the default value 1 and must be unique
 --name VARCHAR(256)
 CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id` INT DEFAULT 1 UNIQUE,
+    `id` INT DEFAULT 1,
+    UNIQUE (ID),
     `name` VARCHAR(256)
 );
