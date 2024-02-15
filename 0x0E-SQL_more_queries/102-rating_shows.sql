5f997f3aedb096f0e7a2f24272cc26a4d262809d
wambetsa <wambetsae@gmail.com>
2024-02-15 01:40:03 -0800

    Show rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,4 @@
---a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+--a script that lists shows from hbtn_0d_tvshows_rate by their rating.
    --Each record should display: tv_shows.title - rating sum
    --Results must be sorted in descending order by the rating
    --You can use only one SELECT statement
@@ -6,7 +6,6 @@
 
 SELECT `title`, SUM(`rate`) AS `rating`
   FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
+       INNER JOIN `tv_show_ratings` AS r ON t.`id` = r.`show_id`
  GROUP BY `title`
  ORDER BY `rating` DESC;
