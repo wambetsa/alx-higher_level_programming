870d9f4d492ca42fcbe460deca0b08fc473b9831
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-15 03:23:37 +0300

    sql more queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,10 @@
---a script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+   -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+   -- Each record should display: tv_shows.title
+   -- Results must be sorted in ascending order by the show title
+   -- You can use only one SELECT statement
+   -- The database name will be passed as an argument of the mysql command
+
 SELECT a.title
 FROM tv_shows AS a
 JOIN tv_show_genres AS b
