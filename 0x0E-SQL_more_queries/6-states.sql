94e57ca73521cbbf14c642711cafc79820c2162d
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-15 03:06:31 +0300

    sql more queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,10 @@
---a script that creates the database hbtn_0d_usa and 
---the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates the database hbtn_0d_usa and table states on my MySQL server.
+   -- states description:
+      -- id INT unique - auto generated, cannot be null, is a primary key
+      -- name VARCHAR(256) - cannot be null
+   -- If the table hbtn_0d_usa already exists, script shouldn't fail
+   -- If the table states already exists, script shouldn't fail
+
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (`id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
