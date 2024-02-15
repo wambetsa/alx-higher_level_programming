1cdb198199ee625060fd86e1f4895e491e097290
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-15 03:25:17 +0300

    sql more queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 --a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-    --Each record should display: tv_shows.title - rating sum
-    --Results must be sorted in descending order by the rating
-    --You can use only one SELECT statement
-    --The database name will be passed as an argument of the mysql command
+   --Each record should display: tv_shows.title - rating sum
+   --Results must be sorted in descending order by the rating
+   --You can use only one SELECT statement
+   --The database name will be passed as an argument of the mysql command
 
 SELECT a.title, SUM(b.rate) AS rating
 FROM tv_shows AS a
