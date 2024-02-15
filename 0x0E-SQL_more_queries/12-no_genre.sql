3028bf58cbb9f6bb6affbc72ab9b81fb09f221cb
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-15 03:19:02 +0300

    sql more queries

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,9 @@
---a script that lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+   -- Each record should display: tv_shows.title - tv_show_genres.genre_id
+   -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+   -- You can use only one SELECT statement
+   -- The database name will be passed as an argument of the mysql command
+
 SELECT a.title, b.genre_id
 FROM tv_shows AS a
 LEFT JOIN tv_show_genres AS b
