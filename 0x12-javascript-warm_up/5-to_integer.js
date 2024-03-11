53ebd3a96db0029ba73cecfd2502d5c25ddb7228
wambetsa emmanuel <wambetsae@gmail.com>
2024-03-12 01:15:02 +0300

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,17 @@
 #!/usr/bin/node
 // Prints two arguments passed to it, in the following format: “ is ”
 
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+// Get the first argument from the command line
+const arg = process.argv[2];
+
+// Convert the argument to an integer using parseInt
+const num = parseInt(arg);
+
+// Check if the argument can be converted to an integer
+if (!isNaN(num)) {
+  // If it can be converted, print "My number: <integer>"
+  console.log(`My number: ${num}`);
+} else {
+  // If it can't be converted, print "Not a number"
+  console.log("Not a number");
+}
