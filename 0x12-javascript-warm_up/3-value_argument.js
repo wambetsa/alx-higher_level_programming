36c317ffcd641f37fae50e19416fa4e02e32feb5
wambetsa emmanuel <wambetsae@gmail.com>
2024-03-12 01:04:00 +0300

    2 to 13

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 // prints the first argument passed
 
-if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
+const arg = process.argv[2];
+if (arg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
