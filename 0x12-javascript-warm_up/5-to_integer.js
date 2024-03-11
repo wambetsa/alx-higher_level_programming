52b19806050569b47b4086a1db5ce48571798939
wambetsa emmanuel <wambetsae@gmail.com>
2024-03-12 01:13:13 +0300

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 // Prints two arguments passed to it, in the following format: “ is ”
 
-if (isNaN(process.argv[2])) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + parseInt(process.argv[2]));
-  }
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
