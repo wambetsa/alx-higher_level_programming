33461e1afc7b6d07b6030a3da7428014ed9fbbb4
wambetsa <wambetsae@gmail.com>
2024-03-12 20:28:30 +0300

    6 multi line loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// prints three lines: (like 1-multi_languages.js) but by using an array of string and a loop
-
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (array[i]) {
+  console.log(array[i]);
+  i++;
 }
