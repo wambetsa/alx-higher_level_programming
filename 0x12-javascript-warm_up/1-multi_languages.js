8052d10d1b9ff57629fc23181932b318c5689c75
wambetsa <wambetsae@gmail.com>
2024-03-12 20:07:00 +0300

    adding 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// It prints 3 lines separately
-
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun');
+console.log('Python is coo');
+console.log('JavaScript is amazing');
