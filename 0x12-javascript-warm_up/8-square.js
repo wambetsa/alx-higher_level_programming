d8b13ed5d46985bad36917893e7be94e0667dbb7
wambetsa <wambetsae@gmail.com>
2024-03-12 20:36:36 +0300

    8 square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-// a script that prints a square
-
-if (isNaN(process.argv[2])) {
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('X'.repeat(parseInt(process.argv[2])));
+  for (let square = 0; square < size; square++) {
+    let row = '';
+    for (let column = 0; column < size; column++) { row += 'X'; }
+    console.log(row);
   }
 }
