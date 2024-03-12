1a6f805958e84b138ecb2fabcf650576a19f3769
wambetsa <wambetsae@gmail.com>
2024-03-12 19:31:40 +0300

    a function that converts a number from base 10 to another base passed as argument

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+A function that converts a number from base 10 to another base passed as argument:
+Prototype: exports.converter = function (base)
+You are not allowed to import any file
+You are not allowed to declare any new variable (var, let, etc..)
+*/
+exports.converter = function (base) {
+  // using closure
+  return function (num) {
+    return num.toString(base);
+  };
+};
