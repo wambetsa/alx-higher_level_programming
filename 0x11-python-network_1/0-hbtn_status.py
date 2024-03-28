61b421123f5813cbba00f878fdf8331f374a9647
root <root@Jewstechs.localdomain>
2024-03-28 20:08:26 +0300

    added readme.md, hbtn_header and hbtn_status files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-    A python script that fetches https://alx-intranet.hbtn.io/status
+    A python script that fetches
+    https://alx-intranet.hbtn.io/status
 """
+
 
 if __name__ == '__main__':
     import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
+
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         result = res.read()
