31515d3da4525735fa245a3a63f254cd02e93fd0
root <root@Jewstechs.localdomain>
2024-03-29 00:40:51 +0300

    added hbtn_header requests file

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,6 +7,7 @@
 if __name__ == '__main__':
     import sys
     import requests
+
     url = sys.argv[1]
     req = requests.get(url)
-    print(req.headers['X-Request-Id'])
+    print(req.headers.get('X-Request-Id'))
