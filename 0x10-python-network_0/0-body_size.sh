c1b2c4824967236a080cf404a417f05615004e15
root <root@Jewstechs.localdomain>
2024-03-28 04:52:26 +0300

    added body_size file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Bash script that takes in URL, sends request to that URL
 # and displays the size of the body of the response
-# curl -s "$1" | wc -c
-curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
+curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2
 
