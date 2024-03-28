7605c4c4cca4b3aff231b645e57da44d4ee63a03
root <root@Jewstechs.localdomain>
2024-03-28 05:39:04 +0300

    added post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script taking url sends post request and shows body response
-curl -sX POST "$1"?email=test@gmail.com&subject=I will always be here for PLD
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
