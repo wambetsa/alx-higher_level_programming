7cf058bd56eef3622215b75cd42019a0f2f45021
root <root@Jewstechs.localdomain>
2024-03-29 02:00:28 +0300

    added json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,11 +9,16 @@ if __name__ == '__main__':
     import sys
     import requests
 
-    if sys.argv <= 1:
-        q = ''
+    if len(sys.argv) == 1:
+        search_key = ''
     else:
-        q = sys.argv[1]
-    url = 'http://0.0.0.0:5000/search_user?{q}'
-    res = requests.post(url)
-    if isinstance(res.json(), json):
-        print("[{}] {}".format(res.json().id, res.json().name))
+        search_key = sys.argv[1]
+
+    payload = {'q': search_key}
+    url = 'http://0.0.0.0:5000/search_user'
+    res = requests.post(url, data=payload)
+
+    if isinstance(res.json(), dict):
+        print("[{}] {}".format(res.get('id'), res.get('name'))
+    else:
+        print('No result')
