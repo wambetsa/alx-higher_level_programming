c21ec38df082b8bb3177543998c539db0b4849b7
root <root@Jewstechs.localdomain>
2024-03-29 02:38:54 +0300

    added my github file

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    a Python script that takes your GitHub credentials
+    (username and password) and uses the GitHub API to display your id
+"""
+
+
+if __name__== '__main__':
+    import sys
+    import requests
+    from requests.auth import HTTPBasicAuth
+
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    url = "https://api.github.com/user"
+    res = requests.get(url, auth=auth)
+    print(res.json().get('id'))
