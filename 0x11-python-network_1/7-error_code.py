9ed149589fc514e9e2677cf203cca1f0e0339e85
root <root@Jewstechs.localdomain>
2024-03-29 01:33:13 +0300

    added requests error code file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+     a Python script that takes in a URL
+     sends a request to the URL
+     and displays the body of the response
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print("{Error code: }".format(res.status_code))
